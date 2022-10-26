c868682a3b41d032fe3a6a75590bd419c5228fea
Samvel <samveltorosyan90@gmail.com>
2022-10-26 16:36:39 +0400

    add Header fix

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
 
     return (
         <div className="App">
-            <header />
+            <Header />
             <button onClick={onToggleButton}>toggle</button>
         </div>
     );
