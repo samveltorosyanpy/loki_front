4b069097ebac8409ba73e93cf4137bc887709e83
Samvel <samveltorosyan90@gmail.com>
2022-10-26 16:03:25 +0400

    init header

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
 
     return (
         <div className="App">
-            <button onClick={onToggleButton}>toggle</button>
+            <header />
             <button onClick={onToggleButton}>toggle</button>
         </div>
     );
